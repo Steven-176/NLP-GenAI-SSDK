20e1294a1a954bfe172e27f76276b6ebf5e0a3d6
DenisKIRAT <denis.kirat@gmail.com>
2024-12-06 17:51:08 +0100

    Rework Projet 1 - Pretreatment and clustering

---
+++ pretraitement.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import spacy
+import re
+import json
+
+reviews_file_path = './reviews.jsonl'
+
+reviews_df = pd.read_json(reviews_file_path, lines=True)
+
+selected_fields = reviews_df[['title', 'text']].dropna()
+
+documents = (selected_fields['title'] + " " + selected_fields['text']).tolist()
+
+nlp = spacy.load("en_core_web_sm")
+
+def clean_text_before_tokenization(doc):
+
+    doc = re.sub(r"<.*?>", " ", doc)
+
+    doc = re.sub(r"\b\w{1,2}\b", " ", doc)
+
+    doc = re.sub(r"\s+", " ", doc).strip()
+    return doc
+
+documents = [clean_text_before_tokenization(doc) for doc in documents]
+
+lemmatized_documents = [[token.lemma_.lower() for token in nlp(doc)] for doc in documents]
+
+#print(lemmatized_documents[:3])
+
+print("\n---------------------------------------------\n")
+
+filtered_tokens = [
+    [
+        token.text for token in nlp(" ".join(doc)) 
+        if not token.is_stop and
+        not token.is_punct and
+        len(token) > 2 and
+        not token.is_digit and
+        not token.like_url and
+        not token.like_email and
+        token.is_alpha
+    ] 
+    for doc in lemmatized_documents]
+
+#print(filtered_tokens[:3])
+
+#print(cleaned_documents[:3])
+
+print("\n---------------------------------------------\n")
+
+print(filtered_tokens[:5])
+
+print("\n---------------------------------------------\n")
+
+output_file = "pretreated_reviews.json"
+
+with open(output_file, "w", encoding="utf-8") as f:
+    json.dump(filtered_tokens, f, ensure_ascii=False)
+
+print(f"Données sauvegardées dans le fichier : {output_file}")
