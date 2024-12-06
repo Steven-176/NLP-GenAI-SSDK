8c6509481dbd42118e15688a4f01638ec96670b7
Steven Sivakaran <sivakaran19@hotmail.fr>
2024-12-06 18:47:36 +0100

    try to launch rag but insufficient quota

--- Projet 2/rag_openai_test.py
+++ Projet 2/rag_openai_test.py
@@ -1,13 +1,12 @@
 import json
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-# from langchain.embeddings import HuggingFaceEmbeddings
-# from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_huggingface import HuggingFaceEmbeddings
-# from langchain.vectorstores import FAISS
 from langchain_community.vectorstores import FAISS
-# from langchain.chat_models import ChatOpenAI
 from langchain_openai import ChatOpenAI
-# from langchain_chroma import Chroma
+# from langchain.chains import RetrievalQA
+from langchain.chains.retrieval import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain import hub
 import os
 from dotenv import load_dotenv
 
@@ -69,11 +68,23 @@ embedding_model = HuggingFaceEmbeddings(model_name=MODEL_NAME)
 #     print(f"Embedding {i + 1}: {embedding[:10]}...")  # Afficher les 10 premières valeurs de l'embedding
 #     print(f"Taille de l'embedding : {len(embedding)}")
 
-# 3. Créer la base vectorielle avec FAISS
+# 4. Créer la base vectorielle avec FAISS
 vectorstore = FAISS.from_texts(
     texts=chunks,
     embedding=embedding_model
 )
 
-# 4. Configurer le modèle LLM
+# 5. Configuration du système de récupération (retriever)
+retriever = vectorstore.as_retriever()
+
+retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
+
+# 6. Configurer le modèle LLM
 llm = ChatOpenAI(model="gpt-3.5-turbo", openai_api_key=OPENAI_API_KEY)
+
+combine_docs_chain = create_stuff_documents_chain(
+    llm, retrieval_qa_chat_prompt
+)
+retrieval_chain = create_retrieval_chain(retriever, combine_docs_chain)
+
+retrieval_chain.invoke({"input": "Combien d'avis ont le mot 'great'"})
